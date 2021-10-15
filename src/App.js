67c09f1e56e154612b82791324960a7a8793138c
duonghungit24 <hungduonng721@gmail.com>
2021-10-15 17:55:50 +0700

    useState bi re render nhieu lan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const cost = [1000 , 2000, 3000, 4000];
+  const total = cost.reduce((number, value) => number + value);  //tinh total de lm initialValue cho useSate
+  const [price , setPrice] = useState(total) //khoi tao total
+  const handlePrice = () => {
+    setPrice(price + 1000)
+  }
+  console.log(total); //sẽ bị re render nhiều , cách khắc phục dùng call back cho useSate
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{price}</h1>
+      <button onClick={handlePrice}>click</button>
     </div>
   );
 }
