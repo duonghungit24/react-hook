a548207bee179a284ccb5482b808f646fd7e4717
duonghungit24 <hungduonng721@gmail.com>
2021-10-16 00:40:20 +0700

    two way binding voi checkbox

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const courses = [
   {
-    id:1,
-    name:'javascript'
+    id: 1,
+    name: "javascript",
   },
   {
-    id:2,
-    name:'react-native',
+    id: 2,
+    name: "react-native",
   },
   {
-    id:3,
-    name:'react'
-  }
-]
+    id: 3,
+    name: "react",
+  },
+];
 function App() {
-  const [checked, setChecked] = useState();
+  const [checked, setChecked] = useState([]);
+  const handleChange = (id) => {
+    const isCheck = checked.includes(id); //kiem tra xem check da co id nao giong vs id truyen chua
+    setChecked(prev => {
+      if(isCheck) { 
+        return  checked.filter(item => item !==id) //loc ra id muon huy  bo va  tra ve mang id moi
+      }
+      else{
+        return [...prev, id]
+      }
+    })
+  };
   const handleSubmit = () => {
-      console.log(checked)
-  }
+    console.log(checked);
+  };
   return (
     <div className="App">
       {courses.map((course) => (
         <div key={course.id}>
-            <input 
-              checked = {checked === course.id}
-              type ='radio'
-              onChange={() => setChecked(course.id)}
-            />
-            {course.name}
+          <input
+            checked={checked.includes(course.id)}
+            type="checkbox"
+            onChange={() => handleChange(course.id)}
+          />
+          {course.name}
         </div>
       ))}
-      <button onClick={handleSubmit} >submit</button>
+      <button onClick={handleSubmit}>submit</button>
     </div>
   );
 }
