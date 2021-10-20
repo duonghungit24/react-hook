0b2232e09a00ffca55cf6d5514d61d2746eac018
duonghungit24 <hungduonng721@gmail.com>
2021-10-20 16:08:33 +0700

    useEffect with deps

--- src/Hook.js
+++ src/Hook.js
@@ -1,27 +1,35 @@
 import { useState, useEffect } from "react";
-//su dung useEffect 
+//su dung useEffect
 //1 useEffect(callback)
 //-goi lai moi lan khi component re-render
 //- goi callback sau khi component them element vao dom
 //2 useEffect(callback,[])
 //- chi goi call back  1 lan sau khi component render
-//3 useEffect(callback,[deps]) 
-//
+//3 useEffect(callback,[deps])
+//callback se goi lai khi deps thay doi
 // tat ca useEffect luon duoc goi sau khi component mounted
+const listChange = ["posts", "albums", "todos", "photos"];
 const Content = () => {
-    const [title, setTitle] = useState('');
-    //document.title = title; neu k su dung useEffect thi no se thuc hien theo thu tu
-     useEffect(() => {     //component se dc goi la thuc hien theo thu tu khi den useEffect thi se vao callback va di chuyen tiep render ra giao dien rooi moi goi den call back
-         document.title = title; 
-     })
-    
-    return (
-        <div>
-            <input
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+  const [change, setChange] = useState("posts");
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${change}`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, [change]);
+  return (
+    <div>
+      {listChange.map((item) => (
+        <button 
+            onClick={() => setChange(item)}
+            key={item}>
+                {item}
+        </button>
+      ))}
+      {data.map((item) => (
+               <li key={item.id}>{item.title}</li>
+           ))}
+    </div>
+  );
+};
 export default Content;
