ec2d37439d0ee2be99aab404b5c6b00f815247d1
duonghungit24 <hungduonng721@gmail.com>
2021-11-10 23:53:38 +0700

    dom events

--- src/Hook.js
+++ src/Hook.js
@@ -8,15 +8,27 @@ import { useState, useEffect } from "react";
 //3 useEffect(callback,[deps])
 //callback se goi lai khi deps thay doi
 // tat ca useEffect luon duoc goi sau khi component mounted
+//clean up function luoon duoc goi truoc khi component unmounted
 const listChange = ["posts", "albums", "todos", "photos"];
 const Content = () => {
   const [data, setData] = useState([]);
   const [change, setChange] = useState("posts");
+  const [showButton, setShowButton] = useState(false);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${change}`)
       .then((response) => response.json())
       .then((data) => setData(data));
+
   }, [change]);
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowButton(window.scrollY >= 300) //viet tat cua if else
+    }
+    window.addEventListener('scroll',handleScroll)
+    //cleanup function 
+    return () => {
+        window.removeEventListener('scroll',handle);
+    }
   return (
     <div>
       {listChange.map((item) => (
@@ -29,6 +41,11 @@ const Content = () => {
       {data.map((item) => (
                <li key={item.id}>{item.title}</li>
            ))}
+      {showButton && (
+        <button
+          style={{position:'fixed', bottom:10, right: 10}}
+        >go to top</button>
+      )}
     </div>
   );
 };
