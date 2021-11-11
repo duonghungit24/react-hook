ac0e95e528f9df611604eb35bde1110da20bb129
duonghungit24 <hungduonng721@gmail.com>
2021-11-11 10:56:16 +0700

    luyen tap useEffect

--- src/Hook.js
+++ src/Hook.js
@@ -7,29 +7,28 @@ import { useState, useEffect } from "react";
 //- chi goi call back  1 lan sau khi component render
 //3 useEffect(callback,[deps])
 //callback se goi lai khi deps thay doi
-// tat ca useEffect luon duoc goi sau khi component mounted
-//clean up function luoon duoc goi truoc khi component mounted
-
+// 1 tat ca useEffect luon duoc goi sau khi component mounted
+// 2 clean up function luoon duoc goi truoc khi component mounted
+// 3 clean up function luoon duoc goi truoc khi callback duoc goi (tru lan mounted)
 
 const Content = () => {
-  const [countdown, setCountdown] = useState(200);
-  //set timeout
-//  useEffect(() => {
-//  const set =  setTimeout(() => {
-//     setCountdown(countdown-1);
-//   },1000)
-//   return () => clearTimeout(set);
-//  },[countdown])
-//set interval
- useEffect(() => {
-   const setCount = setInterval(() => {
-    setCountdown(prev => prev - 1);
-  },1000)
-  return () => clearInterval(setCount)
- })
+  const [avatar, setAvatar] = useState();
+  useEffect(() => {
+    console.log("mounted");
+    return () => {
+        URL.revokeObjectURL(avatar?.preview);
+        console.log("remove")
+    }
+  },[avatar])
+  const handleAvatar = (e) => {
+    const file = e.target.files[0]; // co the chon nhieu anh
+    file.preview = URL.createObjectURL(file); //tao ra 1 url và thêm thuộc tính preview cho object file
+    setAvatar(file);
+  };
   return (
     <div>
-       {countdown}
+      <input type="file" onChange={handleAvatar} />
+      {avatar && <img src={avatar.preview} alt="hello" width="80%"/>}
     </div>
   );
 };
